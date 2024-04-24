922ebf417fdc326212d7f25c9406e4054d5a0d79
Balaji Yadav <93571474+Balaji798@users.noreply.github.com>
2024-04-24 12:48:55 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 import BusBooking from "./pages/BusBooking";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.scss";
-import Home from "./pages/Home";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
         <Route path="/busbooking" element={<BusBooking />} />
       </Routes>
     </BrowserRouter>
