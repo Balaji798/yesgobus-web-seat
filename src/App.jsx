af74f822cd45244e846479ec74ee2c8ca3003601
Balaji Yadav <93571474+Balaji798@users.noreply.github.com>
2024-04-24 18:08:29 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/busbooking" element={<BusBooking />} />
+        <Route path="/" element={<BusBooking />} />
       </Routes>
     </BrowserRouter>
   );
